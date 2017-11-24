4ab5e3af1b647e971c22ab4fdcb0751c09049841
stachen <stanleychen24w@gmail.com>
2017-11-23 22:43:40 -0800

    Check for exit code

--- VisualStudio2017DarkThemeInHighContrastWindowsTheme/EnableVS2017DarkThemeForWindowsHighContrastTheme.ps1
+++ VisualStudio2017DarkThemeInHighContrastWindowsTheme/EnableVS2017DarkThemeForWindowsHighContrastTheme.ps1
@@ -1,25 +1,42 @@
 ﻿## This script will allow you to still use Visual Studio Dark Theme even in Windows High Contrast Theme 
 ## Needs to be run in administrator mode
 
+function CheckExitCode
+{
+    if($LASTEXITCODE -ne 0)
+    {
+        write-error 'Operation failed'
+    }
+}
+
+$ErrorActionPreference = "Stop"
+
 #modify this 
 $VSVersion = '15.0_0202864a'
 
-# load VS private registry 
+write-host 'Loading VS private registry into hive...'
 $binLocation = $env:LOCALAPPDATA + '\Microsoft\VisualStudio\'+ $VSVersion +'\privateregistry.bin'
 $HiveKey = 'HKLM\HKEY_TEMPVS\'
 reg load $HiveKey $binLocation
+CheckExitCode
 
-# export Visual Studio dark theme registry key
+
+write-host 'Exporting Visual Studio dark theme registry key...'
 $ExportKeyFilename = $env:TEMP + '\exportedkey.bak.reg'
 $ImportKeyFilename = $env:TEMP + '\importedkey.reg'
 reg export 'HKEY_LOCAL_MACHINE\HKEY_TEMPVS\Software\Microsoft\VisualStudio\15.0_0202864a_Config\Themes\{1ded0138-47ce-435e-84ef-9ec1f439b749}' $ExportKeyFilename /y
+CheckExitCode
 
-# replace VS dark theme guid with windows high contrast theme guid and import a dark theme setting for the high contrast theme
+write-host 'Replacing VS dark theme guid with windows high contrast theme guid...'
 (Get-Content $ExportKeyFilename).replace('1ded0138-47ce-435e-84ef-9ec1f439b749', 'a5c004b4-2d4b-494e-bf01-45fc492522c7') | Set-Content $ImportKeyFilename
-reg import $ImportKeyFilename
 
-# clean up
+write-host 'Importing a dark theme setting for the high contrast theme...'
+reg import $ImportKeyFilename
+CheckExitCode
+
+write-host 'Cleaning up...'
 [gc]::collect() 
-reg unload $HiveKey
 Remove-Item $ExportKeyFilename
 Remove-Item $ImportKeyFilename
+reg unload $HiveKey
+CheckExitCode
